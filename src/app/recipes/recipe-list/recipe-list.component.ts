968d768db85bea37fac1822f5a0d8ba51b4ec950
Lucy Davidson <lucy.davidson@outlook.com>
2022-02-01 15:19:09 -0500

    recipe services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+
+   }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
-
-  }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,44 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Fettucine Chicken Alfredo',
+    'A wonderful and standard Italian dish',
+    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg',
+    [
+      new Ingredient('Chicken', 20),
+      new Ingredient('Fettucine', 1),
+      new Ingredient('Alfredo', 1)
+
+    ]),
+    new Recipe('Another Test Recipe',
+    'This is simply a test',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWAzFGKqZ-eyjhDJAzoCcBPkC01R-Po37pbg&usqp=CAU',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('Cheese', 1),
+      new Ingredient('Onion', 1),
+      new Ingredient('Sour Cream', 1),
+      new Ingredient('Hard Shells', 12)
+
+    ])
+  ];
+
+  constructor(private slService: ShoppingListService){}
+
+
+getRecipes(){
+  return this.recipes.slice();
+}
+
+addIngredToShopList(ingredients: Ingredient[]){
+  this.slService.addIngredients(ingredients);
+}
+
+}
