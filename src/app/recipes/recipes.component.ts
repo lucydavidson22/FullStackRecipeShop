b0d240be55536db80160dfe65ff60186b3f10680
Lucy Davidson <lucy.davidson@outlook.com>
2022-02-15 19:44:17 -0500

    adding observables

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,14 +11,14 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe:Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    )
+    // this.recipeService.recipeSelected.subscribe(
+    //   (recipe:Recipe) => {
+    //     this.selectedRecipe = recipe;
+    //   }
+    // );
   }
 
 }
