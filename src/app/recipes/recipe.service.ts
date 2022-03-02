2feeb0d62331a0c1db0bf31d7e170210438575fe
Lucy Davidson <lucy.davidson@outlook.com>
2022-03-02 01:50:35 -0500

    adding w9 work

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,32 +7,39 @@ import { Subject } from "rxjs";
 @Injectable()
 export class RecipeService{
   recipesChanged = new Subject<Recipe[]>();
-  // recipeSelected = new Subject<Recipe>();
+  recipeSelected = new Subject<Recipe>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Fettucine Chicken Alfredo',
-    'A wonderful and standard Italian dish',
-    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg',
-    [
-      new Ingredient('Chicken', 20),
-      new Ingredient('Fettucine', 1),
-      new Ingredient('Alfredo', 1)
+  // private recipes: Recipe[] = [
+  //   new Recipe('Fettucine Chicken Alfredo',
+  //   'A wonderful and standard Italian dish',
+  //   'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg',
+  //   [
+  //     new Ingredient('Chicken', 20),
+  //     new Ingredient('Fettucine', 1),
+  //     new Ingredient('Alfredo', 1)
 
-    ]),
-    new Recipe('Another Test Recipe',
-    'This is simply a test',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWAzFGKqZ-eyjhDJAzoCcBPkC01R-Po37pbg&usqp=CAU',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('Cheese', 1),
-      new Ingredient('Onion', 1),
-      new Ingredient('Sour Cream', 1),
-      new Ingredient('Hard Shells', 12)
+  //   ]),
+  //   new Recipe('Another Test Recipe',
+  //   'This is simply a test',
+  //   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWAzFGKqZ-eyjhDJAzoCcBPkC01R-Po37pbg&usqp=CAU',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('Cheese', 1),
+  //     new Ingredient('Onion', 1),
+  //     new Ingredient('Sour Cream', 1),
+  //     new Ingredient('Hard Shells', 12)
 
-    ])
-  ];
+  //   ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService){}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
 
 getRecipes(){
